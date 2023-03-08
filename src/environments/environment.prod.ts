de2bdca60b032cac951d664a80bb27a351becdc5
Rodrigo Lima <rodrigo.lima@b2ml.com.br>
2023-03-08 16:48:31 -0300

    <feat> Alterando o protocolo do link da api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   host: 'https://rldrodrigo.github.io/analise-de-investimentos/',
-  api: 'https://ec2-18-231-91-106.sa-east-1.compute.amazonaws.com:5000/api'
+  api: 'http://ec2-18-231-91-106.sa-east-1.compute.amazonaws.com:5000/api'
 };
