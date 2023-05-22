d38744c825c2d3db3c8484d641163e6f2b21b493
Rodrigo Lima <rodrigo.lima@b2ml.com.br>
2023-05-22 02:44:01 -0300

    <feat> Criação do docker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: true,
-  host: 'https://rldrodrigo.github.io/analise-de-investimentos/',
-  api: 'http://ec2-18-231-91-106.sa-east-1.compute.amazonaws.com:5000/api'
+  host: 'http://localhost:4200',
+  api: 'http://localhost:5000/api',
+  // host: 'https://rldrodrigo.github.io/analise-de-investimentos/',
+  // api: 'http://ec2-18-231-91-106.sa-east-1.compute.amazonaws.com:5000/api'
 };
